e0ef0c8f31d25156a94767cbe244b8f4c7aa2326
NavileenXD <109122203+NavileenXD@users.noreply.github.com>
2025-04-08 17:15:16 -0400

    no idea

--- 3. Advanced-Course/lib/dbdesign.dart
+++ 3. Advanced-Course/lib/dbdesign.dart
@@ -1,3 +1,17 @@
-void dbdesign(){
+import 'dart:io';
+import 'package:postgres/postgres.dart';
 
+void dbdesign() async {
+  final conn = await Connection.open(Endpoint(
+    host: 'localhost',
+    database: 'Perfumes',
+    username: 'postgres',
+    password: 'grupo4',
+  ));
+
+  final result = await conn.execute(
+    Sql.named('SELECT * FROM Tendencia'),
+  );
+  print(result.first.toColumnMap());
+  exit(0);
 }
